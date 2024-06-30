b021b60b7ccfaa196990f257e33ad5522cd7c3cc
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2024-06-30 21:28:39 +0700

    add content to sidebrar

--- src/body/sidebar.tsx
+++ src/body/sidebar.tsx
@@ -3,14 +3,33 @@ export default function Sidebar() {
 
     <div className="sidebar-container">
       <form>
-        <input type="radio" name="restuarant-category" value='restuarant' />
-        <label htmlFor="restuarant-category">ร้านอาหาร</label>
-        <input type="radio" name="beautyAndSpa-catagory" value='beautyAndSpa' />
-        <label htmlFor="beautyAndSpa">ร้านเสริมสวย และ สปา</label>
-        <input type="radio" name="hotel-category" value='hotel' />
-        <label htmlFor="hotel-category">ที่พัก</label>
-        <input type="radio" name="tourist" value='tourist' />
-        <label htmlFor="tourist">สถานที่ท่องเที่ยว</label>
+        <input type="radio" name="restuarant-category" value='restuarant' className="sidebarItem radio" />
+        <label htmlFor="restuarant-category" className="sidebarItem text">ร้านอาหาร</label>
+        <input type="radio" name="beautyAndSpa-catagory" value='beautyAndSpa' className="sidebarItem radio" />
+        <label htmlFor="beautyAndSpa" className="sidebarItem text" >ร้านเสริมสวย และ สปา</label>
+        <input type="radio" name="hotel-category" value='hotel' className="sidebarItem radio" />
+        <label htmlFor="hotel-category" className="sidebarItem text" >ที่พัก</label>
+        <input type="radio" name="tourist" value='tourist' className="sidebarItem radio" />
+        <label htmlFor="tourist" className="sidebarItem text">สถานที่ท่องเที่ยว</label>
+        <hr />
+      </form>
+      <form>
+        <legend>เรตติ้ง</legend>
+        <input type="radio" name="score" value='3.5' />
+        <label htmlFor="restuarant-category">3.5</label>
+        <input type="radio" name="beautyAndSpa-catagory" value='4.5' />
+        <label htmlFor="beautyAndSpa">4.5</label>
+        <hr />
+      </form>
+      <form>
+        <legend>ค้นหาตามระยะห่างจาก</legend>
+        <div className="sidebar-content-container">
+          <input type="text" className="place-input" />
+        </div>
+        <div className="sidebar-content-container">
+          <input type="text" className="place-input" />
+          <img src="../img/dropdown.Small.png" alt="small png" className="icon" />
+        </div>
         <hr />
       </form>
 
