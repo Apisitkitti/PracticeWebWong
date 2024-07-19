4a8a2f263aae17b7e4da172b2632dcea15de70b4
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2024-07-19 21:57:36 +0700

    add foodProfile

---
+++ src/body/FoodProfileCard/foodProfileCard.tsx
@@ -0,0 +1,5 @@
+export default function CardComponent() {
+  return (
+    <div></div>
+  )
+}
--- src/body/PictureCard/PictureCard.tsx
+++ src/body/PictureCard/PictureCard.tsx
@@ -1,16 +1,29 @@
 import { FoodForShow } from "./pictureCardData"
+import { useState } from "react";
 import './pictureCard.css'
 import { pictureAndInformation } from './pictureCardData';
 function PictureCard({ pictureAndInformation }: FoodForShow) {
+  const [isbuttonAppear, setButtonIsAppear] = useState<boolean>(false);
+  const handleButtonAppear = () => {
+    setButtonIsAppear(true);
+  }
+  const handleButtonDisappear = () => {
+    setButtonIsAppear(false);
+  }
   return (
-    <div className="picture-container bodySection">
+    <div className="picture-container bodySection " onMouseEnter={handleButtonAppear} onMouseLeave={handleButtonDisappear}>
       {pictureAndInformation.map((pictureWithCaption) =>
-        <div className="imageAndCaptionBlock">
+        <div className="imageAndCaptionBlock" >
           <p id="pictureCaption">{pictureWithCaption.caption}</p>
           <img id="imageInside" src={pictureWithCaption.picture} alt={pictureWithCaption.caption} />
         </div>
       )}
+      {isbuttonAppear &&
+        <button id="sliderButton">
+          <img id="imageButtonImage" src="../../img/dropdownSmall.png" alt="sliderButton" />
+        </button>}
     </div>
+
   )
 }
 export default function CardComponent() {
