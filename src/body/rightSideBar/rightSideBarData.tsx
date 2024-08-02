2636098926368f0937f0e6515e0d005819df3f6b
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2024-08-02 22:30:45 +0700

    update data

--- src/body/rightSideBar/rightSideBarData.tsx
+++ src/body/rightSideBar/rightSideBarData.tsx
@@ -1,24 +1,27 @@
-import { RestuarantCardType } from "../FoodProfileCard/foodprofileCardData";
+import { RestuarantCardType, restuarantData } from "../FoodProfileCard/foodprofileCardData";
 
 
 interface rightSidebarContent {
   headTitle: string,
   advertise?: ContentTypeInformation[],
-  recommended?: RestuarantCardType[]
+  recommended?: RestuarantCardType[],
+  listRecommended?: needInsideContent[]
 
 }
-
-interface ContentTypeInformation {
-  ad?: boolean,
+interface needInsideContent {
   img: string,
-  name: string,
+  name: string
+}
+interface ContentTypeInformation extends needInsideContent {
+  ad?: boolean,
   place: string;
   bookmark: number;
 }
-
 export const rightSidebarData: rightSidebarContent[] = [
   {
     headTitle: 'ร้านอาหารที่น่าสนใจ',
+    recommended: restuarantData
+
   },
   {
     headTitle: 'บทความแนะนำ',
@@ -29,6 +32,36 @@ export const rightSidebarData: rightSidebarContent[] = [
         name: '“ขนมปังคลีน By Monnaie” ร้านดังจากครัวคุณต๋อย จุดเด่นคือขนมปังคลีน',
         place: 'เมืองอุดรธานี',
         bookmark: 4
-      }]
+      },
+      {
+        ad: false,
+        img: '',
+        name: '“ขนมปังคลีน By Monnaie” ร้านดังจากครัวคุณต๋อย จุดเด่นคือขนมปังคลีน',
+        place: 'เมืองอุดรธานี',
+        bookmark: 4
+      },
+    ]
+  },
+  {
+    headTitle: '',
+    listRecommended: [
+      {
+        img: '',
+        name: ''
+      },
+      {
+        img: '',
+        name: ''
+      },
+      {
+        img: '',
+        name: ''
+      },
+      {
+        img: '',
+        name: ''
+      },
+    ]
+
   }
 ]
