0988e103adaab30d6e9ca04da8b3291f9c7d5153
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2024-09-13 23:36:36 +0700

    now change function to anonymous in slide

--- src/body/PictureCard/PictureCard.tsx
+++ src/body/PictureCard/PictureCard.tsx
@@ -6,30 +6,11 @@ import { pictureAndInformation, PictureType } from './pictureCardData';
 function PictureCard({ pictureAndInformation }: FoodForShow) {
   const [isbuttonAppear, setButtonIsAppear] = useState<boolean>(false)
   const [pageNumber, setPageCount] = useState<number>(0)
-  let showImageSlider = pictureAndInformation.slice(pageNumber, pageNumber + 5)
-  const handleButtonNext = () => {
-    if (pageNumber < pictureAndInformation.length - showImageSlider.length) {
-      setPageCount(pageNumber + 5)
-    }
-    else {
-      return
-    }
-
-  }
-  const handleButtonPrevious = () => {
-    if (pageNumber !== 0) {
-      setPageCount(pageNumber - 5)
-    }
-    else {
-      return
-    }
-  }
-  const handleButtonAppear = () => setButtonIsAppear(true);
-
-  const handleButtonDisappear = () => setButtonIsAppear(false);
+  const picturePerPage: number = 5;
+  const showImageSlider = pictureAndInformation.slice(pageNumber, pageNumber + picturePerPage)
 
   return (
-    <div className="picture-container bodySection " onMouseEnter={handleButtonAppear} onMouseLeave={handleButtonDisappear}>
+    <div className="picture-container bodySection " onMouseEnter={() => setButtonIsAppear(true)} onMouseLeave={() => setButtonIsAppear(false)}>
       {showImageSlider.map((pictureWithCaption: PictureType, index: number) =>
         <div key={index} className="imageAndCaptionBlock" >
           <p id="pictureCaption">{pictureWithCaption.caption}</p>
@@ -39,12 +20,12 @@ function PictureCard({ pictureAndInformation }: FoodForShow) {
       )}
       {pageNumber < pictureAndInformation.length - showImageSlider.length && isbuttonAppear &&
         <button id="sliderButton">
-          <img id="imageButtonImage" src="../../img/dropdownSmall.png" alt="sliderButton" onClick={handleButtonNext} />
+          <img id="imageButtonImage" src="../../img/dropdownSmall.png" alt="sliderButton" onClick={() => setPageCount(pageNumber + picturePerPage)} />
         </button>
       }
       {pageNumber > 0 && isbuttonAppear &&
         <button id="sliderButtonLeft">
-          <img id="imageButtonImage" src="../../img/dropdownSmall.png" alt="sliderButton" onClick={handleButtonPrevious} />
+          <img id="imageButtonImage" src="../../img/dropdownSmall.png" alt="sliderButton" onClick={() => setPageCount(pageNumber - picturePerPage)} />
         </button>
       }
     </div>
