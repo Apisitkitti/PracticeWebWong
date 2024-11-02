09e0104f1165d8ffff15d9b966a370a836638892
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2024-11-02 17:59:39 +0700

    update headliner

--- src/body/headLiner.tsx
+++ src/body/headLiner.tsx
@@ -4,13 +4,15 @@ const HeadLiner = () => {
   return (
     <div className="headLiner-container">
       <div className='headLiner-text-container'>
-        <a href='##'> หน้าแรก </a>
-        <span className='slash'>/</span>
-        <a href='##'> ร้านอาหาร </a>
-        <span className='slash'>/</span>
-        <a href='##'> ร้านอาหารและปริมณฑล </a>
-        <span className='slash'>/</span>
-        <span className='hl'> ค้นหาร้านอาหาร</span>
+        <div className='linkTextGroup'>
+          <a href='##'> หน้าแรก </a>
+          <span className='slash'>/</span>
+          <a href='##'> ร้านอาหาร </a>
+          <span className='slash'>/</span>
+          <a href='##'> ร้านอาหารและปริมณฑล </a>
+          <span className='slash'>/</span>
+          <span className='hl'> ค้นหาร้านอาหาร</span>
+        </div>
         <p className="text-header hl">ร้านอาหาร ยอดนิยม ในกรุงเทพและปริมณฑล</p>
       </div>
     </div>
